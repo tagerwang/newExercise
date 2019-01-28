f055b9d07b8eb8f79cb2e9db2e33ccd186ccbb7d
tager.wang <wangtao3@xhg.com>
2019-01-28 18:04:12 +0800

    angular get & set

--- angular/my-app20181102/src/app/dashboard/dashboard.component.ts
+++ angular/my-app20181102/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Hero } from '../heroes/hero';
 import { HeroService } from '../hero.service';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,7 @@ import { HeroService } from '../hero.service';
   styleUrls: [ './dashboard.component.scss' ]
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+  private _testGetOrSet:any = 0;
   heroes: Hero[] = [];
   public $change: any;
   constructor(private heroService: HeroService) {
@@ -15,15 +17,32 @@ export class DashboardComponent implements OnInit, OnDestroy {
     // if (this.$change) {
     //   this.$change.unsubscribe();
     // }
-    this.$change = this.heroService.subject.subscribe(res => {
+    this.$change = this.heroService.subject.pipe(share()).subscribe(res => {
       console.log('observerA值为：' + res);
     });
+    // this.$change = this.heroService.subject.subscribe(res => {
+    //   console.log('observerA值为：' + res);
+    // });
     // this.heroService.subject.subscribe({
     //   next: (v) => console.log('observerA值为: ' + v)
     // });
   }
   expression () {
     this.heroService.subject.next('observerA123');
+  }
+  get testGetOrSet () {
+    return this._testGetOrSet;
+  }
+  set testGetOrSet (item) {
+    console.log(item);
+    this._testGetOrSet = item + 10;
+  }
+  expression1 () {
+    console.log('get click:', this.testGetOrSet);
+  }
+  expression2 () {
+    this.testGetOrSet += 1;
+    console.log('set click:', this.testGetOrSet);
   }
   ngOnInit() {
     this.getHeroes();
