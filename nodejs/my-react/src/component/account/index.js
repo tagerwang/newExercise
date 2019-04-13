0b120ee36d31d7684fdea8407a0e8bcbeb536822
tager <2121190200@qq.com>
2019-04-13 17:07:02 +0800

    react account

---
+++ nodejs/my-react/src/component/account/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class account extends Component {
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        );
+    }
+}
+
+export default account;
